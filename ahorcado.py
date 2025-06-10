09ae992e75ef76bcd053fe3d8d3cf1608de87324
Agostina Chiara <agosschiara25@gmail.com>
2025-06-09 21:42:53 -0300

    Correccion Analisis Estatico

--- ahorcado.py
+++ ahorcado.py
@@ -58,13 +58,12 @@ class Ahorcado:
             self.juego_terminado = True
             self.victoria = True
             return True
-        else:
-            self.intentos_restantes -= 1
+        self.intentos_restantes -= 1
 
-            if self.intentos_restantes <= 0:
-                self.juego_terminado = True
+        if self.intentos_restantes <= 0:
+            self.juego_terminado = True
 
-                return False
+            return False
 
     def _verificar_victoria(self):
         '''
