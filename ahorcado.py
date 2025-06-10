4caa0b5c74798db88f3796ca37c755309e6cf7c6
Agostina Chiara <agosschiara25@gmail.com>
2025-06-09 21:10:14 -0300

    Correcion Analisis Estatico del Codigo

--- ahorcado.py
+++ ahorcado.py
@@ -10,6 +10,12 @@ class Ahorcado:
     y verifica condiciones de victoria o derrota.
     '''
     def __init__(self, palabra, intentos_maximos=6):
+        '''
+        Inicializa el juego del ahorcado con la palabra secreta y los intentos máximos.
+
+        :param palabra: Palabra a adivinar.
+        :param intentos_maximos: Cantidad máxima de intentos permitidos.
+        '''
         self.palabra_secreta = palabra.lower()
         self.intentos_maximos = intentos_maximos
         self.intentos_restantes = intentos_maximos
@@ -18,6 +24,12 @@ class Ahorcado:
         self.victoria = False
 
     def arriesgar_letra(self, letra):
+        '''
+        Intenta adivinar una letra de la palabra secreta.
+
+        :param letra: Letra que el jugador quiere adivinar.
+        :return: True si la letra está en la palabra, False en caso contrario.
+        '''
         letra = letra.lower()
         self.letras_adivinadas.add(letra)
         if self.juego_terminado:
@@ -30,10 +42,16 @@ class Ahorcado:
 
             if self.intentos_restantes <= 0:
                 self.juego_terminado = True
-          
+
             return False
 
     def arriesgar_palabra(self, palabra_intento):
+        '''
+        Intenta adivinar toda la palabra secreta.
+
+        :param palabra_intento: Palabra que el jugador intenta adivinar.
+        :return: True si la palabra es correcta, False en caso contrario.
+        '''
         palabra_intento = palabra_intento.lower()
 
         if palabra_intento == self.palabra_secreta:
@@ -45,10 +63,13 @@ class Ahorcado:
 
         if self.intentos_restantes <= 0:
             self.juego_terminado = True
-        
-            return False  
+
+            return False
 
     def _verificar_victoria(self):
+        '''
+        Verifica si el jugador ha adivinado todas las letras de la palabra secreta.
+        '''
         for letra in self.palabra_secreta:
             if letra not in self.letras_adivinadas:
                 return
@@ -57,6 +78,11 @@ class Ahorcado:
         self.victoria = True
 
     def palabra_actual(self):
+        '''
+        Muestra el estado actual de la palabra con las letras adivinadas hasta el momento.
+
+        :return: Cadena con letras adivinadas y guiones bajos por letras faltantes.
+        '''
         resultado = ""
         for letra in self.palabra_secreta:
             if letra in self.letras_adivinadas:
